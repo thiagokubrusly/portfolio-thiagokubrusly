a1a747edf1098e750efb632faa20ebe233a8b50a
Thiago Kubrusly <thiago.kfreitas@hotmail.com>
2023-05-24 14:14:22 -0300

    add portfolio base

---
+++ script.js
@@ -0,0 +1,100 @@
+///////// Preview do Slider ////////////
+const imgEstaticaSlider= "./assets/imgEstatica-slider.JPG";
+const imgAnimadaSlider = "./assets/imgAnimada-slider.gif";
+
+function trocarSlider(){
+    document.getElementById("figuraSlider").src = imgAnimadaSlider;
+}
+
+function voltarSlider(){
+    document.getElementById("figuraSlider").src = imgEstaticaSlider;
+}
+
+
+/////// Preview da Calculadora ///////////
+const imgEstaticaCalc= "./assets/imgEstatica-calc.JPG";
+const imgAnimadaCalc = "./assets/imgAnimada-calc.gif";
+
+function trocarCalc(){
+    document.getElementById("figuraCalc").src = imgAnimadaCalc;
+}
+
+function voltarCalc(){
+    document.getElementById("figuraCalc").src = imgEstaticaCalc;
+}
+
+
+///////// Preview do Boardcard ////////////
+const imgEstaticaBoardcard= "./assets/imgEstatica-boardcard.JPG";
+const imgAnimadaBoardcard = "./assets/imgAnimada-boardcard.gif";
+
+function trocarBoardcard(){
+    document.getElementById("figuraBoardcard").src = imgAnimadaBoardcard;
+}
+
+function voltarBoardcard(){
+    document.getElementById("figuraBoardcard").src = imgEstaticaBoardcard;
+}
+
+
+//// Preview do Organizador de Links ////////////
+const imgEstaticaOrganizador= "./assets/imgEstatica-organizador.JPG";
+const imgAnimadaOrganizador = "./assets/imgAnimada-organizador.gif";
+
+function trocarOrganizador(){
+    document.getElementById("figuraOrganizador").src = imgAnimadaOrganizador;
+}
+
+function voltarOrganizador(){
+    document.getElementById("figuraOrganizador").src = imgEstaticaOrganizador;
+}
+
+
+
+///////// Preview do Alurabooks ////////////
+const imgEstaticaAlurabooks= "./assets/imgEstatica-alurabooks.JPG";
+const imgAnimadaAlurabooks = "./assets/imgAnimada-alurabooks.gif";
+
+function trocarAlurabooks(){
+    document.getElementById("figuraAlurabooks").src = imgAnimadaAlurabooks;
+}
+
+function voltarAlurabooks(){
+    document.getElementById("figuraAlurabooks").src = imgEstaticaAlurabooks;
+}
+
+///////// Preview do Robotron ////////////
+const imgEstaticaRobotron= "./assets/imgEstatica-robotron.JPG";
+const imgAnimadaRobotron = "./assets/imgAnimada-robotron.gif";
+
+function trocarRobotron(){
+    document.getElementById("figuraRobotron").src = imgAnimadaRobotron;
+}
+
+function voltarRobotron(){
+    document.getElementById("figuraRobotron").src = imgEstaticaRobotron;
+}
+
+///////// Preview do Aluramid ////////////
+const imgEstaticaAluramid= "./assets/imgEstatica-aluramid.JPG";
+const imgAnimadaAluramid = "./assets/imgAnimada-aluramid.gif";
+
+function trocarAluramid(){
+    document.getElementById("figuraAluramid").src = imgAnimadaAluramid;
+}
+
+function voltarAluramid(){
+    document.getElementById("figuraAluramid").src = imgEstaticaAluramid;
+}
+
+///////// Preview do Barbearia ////////////
+const imgEstaticaBarbearia= "./assets/imgEstatica-barbearia.JPG";
+const imgAnimadaBarbearia = "./assets/imgAnimada-barbearia.gif";
+
+function trocarBarbearia(){
+    document.getElementById("figuraBarbearia").src = imgAnimadaBarbearia;
+}
+
+function voltarBarbearia(){
+    document.getElementById("figuraBarbearia").src = imgEstaticaBarbearia;
+}
